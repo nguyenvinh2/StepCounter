a81a05eae74d2f6d47ae6b631a4888f1fd57672e
nguye <nguyenv2@outlook.com>
2018-12-28 20:45:37 -0800

    need to figure out hardware configuration

---
+++ README.md
@@ -0,0 +1,12 @@
+# Step Counter
+
+## Intro
+
+This project is for creating a step counter (pedometer) Android App using C# and Xamarin in Visual Studios.
+
+Currently Work in Progress
+
+
+## Output
+
+![Console](assets/Capture.PNG)
--- StepCounter/MainActivity.cs
+++ StepCounter/MainActivity.cs
@@ -3,17 +3,59 @@ using Android.OS;
 using Android.Support.V7.App;
 using Android.Runtime;
 using Android.Widget;
+using Android.Hardware;
+using System;
 
 namespace StepCounter
 {
-    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
-    public class MainActivity : AppCompatActivity
+    [Activity(Label = "Step Counter", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity, ISensorEventListener
     {
-        protected override void OnCreate(Bundle savedInstanceState)
+        private SensorManager sensorManager;
+        private TextView steps;
+        private bool Run = false;
+
+        public void OnAccuracyChanged(Sensor sensor, [GeneratedEnum] SensorStatus accuracy)
         {
-            base.OnCreate(savedInstanceState);
+            throw new System.NotImplementedException();
+        }
+
+        public void OnSensorChanged(SensorEvent e)
+        {
+            if (Run)
+            {
+                steps.SetText((int)Math.Ceiling(e.Values[0]));
+            }
+        }
+
+        public new void OnResume()
+        {
+            Run = true;
+            Sensor StepCount = sensorManager.GetDefaultSensor(SensorType.StepCounter);
+            if (StepCount != null)
+            {
+                sensorManager.RegisterListener(this, StepCount, SensorDelay.Ui);
+            }
+            else
+            {
+                Toast.MakeText(this, "No Sensor Found!", ToastLength.Short).Show();
+            }
+        }
+
+        public new void OnPause()
+        {
+            Run = false;
+            sensorManager.UnregisterListener(this);
+        }
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+
+            steps = FindViewById<TextView>(Resource.Id.steps);
+            sensorManager = (SensorManager)GetSystemService(SensorService);
         }
     }
 }
